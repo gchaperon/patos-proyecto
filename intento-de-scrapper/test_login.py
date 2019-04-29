292cdbff2ddf1b8334fd2c7435ec2a1188b85674
gabriel <gabrielchaperonb@gmail.com>
2019-04-29 00:38:31 -0400

    eiiii, esta wea hace algo, es un comienzo

--- intento-de-scrapper/test_login.py
+++ intento-de-scrapper/test_login.py
@@ -1,17 +1,30 @@
 import requests
 import pickle
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 
 # tengo mis datos escondidos, porque obvio
 with open('user_data.pic', 'rb') as f:
   payload = pickle.load(f)
 
+# es importante agregarle esto a la wea que se envia pa poder loguearse
 payload['servicio'] = 'ucursos'
 # payload['debug'] = 0
 
+# esta wea es a logearse con el usuario de cada uno y mantener la sesion
+# abierta pa poder seguir SURFEANDO ucursos
 post_url = 'https://www.u-cursos.cl/upasaporte/adi'
-
 print(payload)
+session = requests.Session()
+response = session.post(post_url, data=payload)
+print(response.status_code)
 
-r = requests.post(post_url, data=payload)
-print(r.status_code)
+
+# ahora es pa empezar a traer comentarios
+first_page = 'https://www.u-cursos.cl/ingenieria/2/foro_institucion/'
+response = session.get(first_page)
+
+# esto es pa parsear el html y poder buscar weas
+soup = BeautifulSoup(response.text)
+comment = soup.find('div', {'id': 'mensaje_2205098'})
+
+print(comment) 
