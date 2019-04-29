fd4b7cf2e6a8bc8e3f10fdc72c15f2f459e7e2d8
gabriel <gabrielchaperonb@gmail.com>
2019-04-29 01:05:54 -0400

    agrega unos cuantos chequeos

--- intento-de-scrapper/test_login.py
+++ intento-de-scrapper/test_login.py
@@ -17,6 +17,7 @@ print(payload)
 session = requests.Session()
 response = session.post(post_url, data=payload)
 print(response.status_code)
+assert response.status_code == 200, 'diablos, deberia ser 200'
 
 
 # ahora es pa empezar a traer comentarios
@@ -24,7 +25,9 @@ first_page = 'https://www.u-cursos.cl/ingenieria/2/foro_institucion/'
 response = session.get(first_page)
 
 # esto es pa parsear el html y poder buscar weas
-soup = BeautifulSoup(response.text)
+soup = BeautifulSoup(response.text, features='html5lib')
 comment = soup.find('div', {'id': 'mensaje_2205098'})
+assert comment is not None, \
+  'diablos, quiza el mensaje que busco ya no esta en la pagina principal'
 
 print(comment) 
