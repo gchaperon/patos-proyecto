4ff588be429a139495ebbd8029a7bd393ca35ac4
Valeria Guidotti <valeria.guidotti@ug.uchile.cl>
2019-05-12 02:40:36 -0400

    watafaka

--- intento-de-scrapper/test_timestamp.py
+++ intento-de-scrapper/test_timestamp.py
@@ -1,5 +1,8 @@
 import datetime as dt
 import re
+import os
+import argparse
+import csv
 
 # no encontre los meses y no me dio paja escribirlos asi que estos son los messesss
 mes = {
@@ -42,9 +45,9 @@ def relative_date_2_timestamp(date_query: dt.datetime, typed_date: str):
                 )
         elif(matchDate.group(1) == 'Ayer'): #restar un dia
             date_timestamp = dt.datetime(
-                (date_query - dt.timedelta(1,0,0,0,0)).year,
-                (date_query - dt.timedelta(1,0,0,0,0)).month,
-                (date_query - dt.timedelta(1,0,0,0,0)).day,
+                (date_query - dt.timedelta(days = 1)).year,
+                (date_query - dt.timedelta(days = 1)).month,
+                (date_query - dt.timedelta(days = 1)).day,
                 int(matchDate.group(2)),
                 int(matchDate.group(3))
                 )
@@ -54,6 +57,11 @@ def relative_date_2_timestamp(date_query: dt.datetime, typed_date: str):
 def date_2_timestamp(date_query: dt.datetime, typed_date: str):
     """
     el timestamp nomasss
+    en caso de encontrarse con un formato de fecha no especificado
+    retorna NO_MATCHED_FORMAT
+    para el caso de los años, se espera que sean todos sobre los 2000
+    si hay uno anterior a esto se guardara como la suma con 1900 (por si fuera 92 o algo asi)
+
     date_query: fecha de la query, para sacar el año
     typed_date: fecha pa timestampear
     """
@@ -62,24 +70,34 @@ def date_2_timestamp(date_query: dt.datetime, typed_date: str):
         matchThisYear = re.match(r'(.*) de (.*)', matchDate.group(1))
         matchOtherYear = re.match(r'(.*)/(.*)/(.*)', matchDate.group(1))
         if(matchThisYear):
-            d = int(matchThisYear.group(1))
+            d = int(matchThisYear.group(1)) if len(matchThisYear.group(1)) <= 2 else int(matchThisYear.group(1).split(' ')[1])
             m = mes[matchThisYear.group(2)]
             y = date_query.year
-        elif(matchOtherYear):
-            d = int(matchOtherYear.group(1))
-            m = int(matchOtherYear.group(2))
-            y = int(matchOtherYear.group(3))
-        try:
             date_timestamp = dt.datetime(
                 year= y,
                 month= m,
                 day= d,
-                hours=int(matchDate.group(2)),
-                minutes=int(matchDate.group(3)))
-
+                hour=int(matchDate.group(2)),
+                minute=int(matchDate.group(3))
+                )
             return date_timestamp.timestamp()
-        except:
-            return 'uwu'
+        elif(matchOtherYear):
+            d = int(matchOtherYear.group(1))
+            m = int(matchOtherYear.group(2))
+            y = int(matchOtherYear.group(3))
+            y = y + 2000 if y < 50 else y + 1900
+            date_timestamp = dt.datetime(
+                year= y,
+                month= m,
+                day= d,
+                hour=int(matchDate.group(2)),
+                minute=int(matchDate.group(3))
+                )
+            return date_timestamp.timestamp()
+        else:
+            return 'NO_MATCHED_FORMAT'
+    else:
+        return 'NO_MATCHED_FORMAT'   
 
 
 def str_2_timestamp(date_query: str, typed_date: str):
@@ -94,7 +112,60 @@ def str_2_timestamp(date_query: str, typed_date: str):
         return relative_date_2_timestamp(date_query_dt, typed_date)
     else:
         return date_2_timestamp(date_query_dt, typed_date)
+
+
+def files_with_timestamp(file, type):
+    """
+    recibe un archivo sobre el cual leer y escribe la copia con el timestamp como float
+    actualmente no escribe los headers en los archivos, hay que descomentarlalinea nomass
+    
+    file: nombre de un archivo tsv sobre el cual leer
+    type: si es ROOT o CHILD para diferenciar headers
+    """
+    with open(file, 'r', encoding="utf-8") as src_file,\
+        open(f'ts_{file}', 'w', encoding="utf-8", newline="") as out_file:
+
+        if(type=="ROOT"):
+            fields = ['id', 'titulo', 'autor', 'fecha_ts', 'tema', 'mensaje']
+        elif(type=="CHILD"):
+            fields = ['id', 'id_th', 'id_p', 'autor', 'fecha_ts', 'mensaje']
         
+        reader = csv.DictReader(
+            src_file,
+            delimiter='\t'
+                )
+        writer = csv.DictWriter(
+            out_file,
+            fieldnames=fields,
+            delimiter='\t'
+            )
+        
+        # writer.writeheader()
+
+        out_array = []
+        for row in reader:
+            temp = {}
+            for field in fields:
+                if field != 'fecha_ts':
+                    temp[field] = row[field]
+            ts = str_2_timestamp(row['current_time'], row['fecha'])
+            temp['fecha_ts'] = ts
+            out_array.append(temp)
+
+        print(f"Escribiendo en el archivo ts_{file}")
+        for line in out_array:
+            writer.writerow(line)
+        
+
+if __name__ == '__main__':
+    for file in os.listdir('.'):
+        if file.endswith(".tsv"):
+            if file.startswith("root_"):
+                files_with_timestamp(file, "ROOT")
+            elif file.startswith("child_"):
+                files_with_timestamp(file, "CHILD")
+        
+
 tiempos_jejemplo = [
     "Hoy, hace 5 mins", 
     "Hoy, hace 13 segs",
