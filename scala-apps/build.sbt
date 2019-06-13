8c80f932e2d2657463446ac4d6dbc5faa0ee6517
gabriel <gabrielchaperonb@gmail.com>
2019-06-13 02:36:39 -0400

    wea

--- scala-apps/build.sbt
+++ scala-apps/build.sbt
@@ -4,4 +4,5 @@ version := "1.0"
 
 scalaVersion := "2.11.12"
 
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.3"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.3"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.3"
--- scala-apps/src/main/scala/PlusOneMinusOne.scala
+++ scala-apps/src/main/scala/PlusOneMinusOne.scala
@@ -1,16 +1,15 @@
-/* SimpleApp.scala */
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
 
 object PlusOneMinusOne {
-    def main(args: Array[String]) {
-        val filePath = "/home/gabriel/2019-1/patos/proyecto/data/root_all_wrepeat_tsdate.tsv"
-        val spark = SparkSession.builder.appName("Count +1/-1").getOrCreate()
-        val rootComments = spark.read.textFile(filePath).cache()
+  def main(args: Array[String]) {
+    val filePath = "/home/gabriel/2019-1/patos/proyecto/data/root_all_wrepeat_tsdate.tsv"
+    val spark = new SparkContext(new SparkConf().setAppName("Count +1/-1"))
 
-        val countAll = rootComments.map(line => line.split("\t")(1))
-                        .filter(title => title.contains("+1/-1"))
-                        .count()
-        println(s"Numero total de encuestas +1/-1: $countAll")
-        spark.stop()
-    }    
+    val rootComments = spark.textFile(filePath)
+
+    val countAll = rootComments.filter(line => line.split("\t").size.equals(1)).count()
+    println(s"Numero total de encuestas +1/-1: $countAll")
+    spark.stop()
+  }    
 }
