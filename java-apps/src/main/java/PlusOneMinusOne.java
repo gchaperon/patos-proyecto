77a69779947945315251d2aa2b96bbdb73b0a1d5
gabriel <gabrielchaperonb@gmail.com>
2019-06-15 01:55:12 -0400

    waaaaa

--- java-apps/src/main/java/PlusOneMinusOne.java
+++ java-apps/src/main/java/PlusOneMinusOne.java
@@ -3,15 +3,26 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.time.Instant;
+import java.util.Date;
+
 public class PlusOneMinusOne {
     public static void main(String[] args) {
         String filePath = "/home/gabriel/2019-1/patos/proyecto/data/root_all_wrepeat_tsdate_fixed.tsv";
         JavaSparkContext spark = new JavaSparkContext(new SparkConf().setAppName("Count +1/-1"));
 
-        JavaRDD<String> inputRDD= spark.textFile(filePath);
-        long countAll = inputRDD.filter(line -> line.split("\t")[1].contains("+1/-1")).count();
+        JavaRDD<String> inputRDD = spark.textFile(filePath);
+        long countPreBan = inputRDD
+            .filter(line -> line.split("\t")[1].contains("+1/-1"))
+            .filter(line -> Double.valueOf(line.split("\t")[3]) < 1530403200).count();
 
-        System.out.println("\t\tMira ctm, la wea es asi" + countAll);
+        long countPostBan = inputRDD
+            .filter(line -> line.split("\t")[1].contains("+1/-1"))
+            .filter(line -> Double.valueOf(line.split("\t")[3]) >= 1530403200).count();
+
+        System.out.println();
+        System.out.println("\t\tMira ctm, la wea es asi " + countPreBan + " y asi " + countPostBan);
+        System.out.println();
 
         spark.close();
     }
