70d1a1fef534fabe929e494e340337c10f0598f3
gabriel <gabrielchaperonb@gmail.com>
2019-06-14 22:13:47 -0400

    debug line test

--- scala-apps/src/main/scala/PlusOneMinusOne.scala
+++ scala-apps/src/main/scala/PlusOneMinusOne.scala
@@ -3,12 +3,12 @@ import org.apache.spark.SparkConf
 
 object PlusOneMinusOne {
   def main(args: Array[String]) {
-    val filePath = "/home/gabriel/2019-1/patos/proyecto/data/root_all_wrepeat_tsdate.tsv"
+    val filePath = "/home/gabriel/2019-1/patos/proyecto/data/root_all_wrepeat_tsdate_fixed.tsv"
     val spark = new SparkContext(new SparkConf().setAppName("Count +1/-1"))
 
     val rootComments = spark.textFile(filePath)
 
-    val countAll = rootComments.filter(line => line.split("\t").size.equals(1)).count()
+    val countAll = rootComments.filter(line => line.split("\t")(1).contains("+1/-1")).count()
     println(s"Numero total de encuestas +1/-1: $countAll")
     spark.stop()
   }
---
+++ scala-apps/src/main/scala/TopCommenters.scala
@@ -0,0 +1,73 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext._
+import java.util._
+
+object TopCommenters {
+  def main(args: Array[String]): Unit = {
+    val rootsPath = "/home/gabriel/2019-1/patos/proyecto/data/root_all_wrepeat_tsdate_fixed.tsv";
+    val childsPath = "/home/gabriel/2019-1/patos/proyecto/data/child_all_wrepeat_tsdate_fixed.tsv";
+    
+    val spark = new SparkContext(new SparkConf().setAppName("Top commenters"));
+    
+    val rootRDD = spark.textFile(rootsPath);
+    val rootWoenAndDateRDD = rootRDD.map(
+      line => (
+        line.split("\t")(2), 
+        (new Date(
+          (line.split("\t")(3).toDouble * 1000L).toLong
+        )).getYear() + 1900
+      )
+    );
+
+    val childRDD = spark.textFile(childsPath);
+    val childWoenAndDateRDD = childRDD.map(
+      line => (
+        line.split("\t")(3),
+        (new Date(
+          (line.split("\t")(4).toDouble * 1000L).toLong
+        )).getYear() + 1900
+      )
+    );
+
+    
+    val superWoenAndDateRDD = rootWoenAndDateRDD.union(childWoenAndDateRDD).cache();
+
+
+    def seqOp = (accumulator: PriorityQueue, element: (String, Long)) => {
+      if(accumulator.length < 10) {
+        accumulator.enqueue(element) 
+      } else {
+        if accumulator.head._2 < element._2 {
+          accumulator.dequeue()
+          return accumulator.enqueue(element)
+        }
+
+
+      }
+    }
+ 
+
+    superWoenAndDateRDD
+      .map(pair => (pair, 1L))
+      .reduceByKey((u, v) => u + v)
+      .map(row => (row._1._2, (row._1._1, row._2)))
+      .aggregateByKey(
+        new PriorityQueue(Ordering.by[(String, Long), Long](- _._2))
+      )()
+      .take(10)
+      .foreach(println)
+
+    // superWoenAndDateRDD.take(10).foreach(println)
+
+
+
+    // val groupedRDD = superWoenAndDateRDD.groupByKey().map(row => (row._1, row._2.size)).sortBy(_._2, false);
+
+    // groupedRDD.take(30).foreach(println);
+
+    println("Shitterton");
+
+    spark.stop()
+  } 
+}
