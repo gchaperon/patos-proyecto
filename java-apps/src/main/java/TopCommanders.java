970a0b848b8b07b9ee9c9d7a6278e9eb9df32788
gabriel <gabrielchaperonb@gmail.com>
2019-06-15 03:46:51 -0400

    por fin ctm

---
+++ java-apps/src/main/java/TopCommanders.java
@@ -0,0 +1,74 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import java.util.*;
+
+public class TopCommanders {
+    public static void main(String[] args) {
+        String rootsPath = "/home/gabriel/2019-1/patos/proyecto/data/root_all_wrepeat_tsdate_fixed.tsv";
+        String childsPath = "/home/gabriel/2019-1/patos/proyecto/data/child_all_wrepeat_tsdate_fixed.tsv";
+
+        JavaSparkContext spark = new JavaSparkContext(new SparkConf().setAppName("Count +1/-1"));
+
+        JavaRDD<String> inputRootsRDD = spark.textFile(rootsPath);
+        JavaPairRDD<String, Integer> rootWoenAndDateRDD = inputRootsRDD.mapToPair(
+            line -> new Tuple2<>(
+                line.split("\t")[2],
+                Utils.yearFromEpochString(line.split("\t")[3])
+            )
+        );
+
+        JavaRDD<String> inputChildsRDD = spark.textFile(childsPath);
+        JavaPairRDD<String, Integer> childWoenAndDateRDD = inputChildsRDD.mapToPair(
+            line -> new Tuple2<>(
+                line.split("\t")[3],
+                Utils.yearFromEpochString(line.split("\t")[4])
+            )
+        );
+
+        JavaPairRDD<String, Integer> superWoenAndDateRDD = rootWoenAndDateRDD.union(childWoenAndDateRDD);
+
+        superWoenAndDateRDD
+            .mapToPair(pair -> new Tuple2<>(pair, 1L))
+            .reduceByKey(Long::sum)
+            .mapToPair(row -> new Tuple2<>(row._1._2, new Tuple2<>(row._1._1, row._2)))
+            .groupByKey()
+            .mapToPair(tup -> {
+                PriorityQueue<Tuple2<String, Long>> pq = new PriorityQueue<>((t1, t2) -> t1._2.compareTo(t2._2));
+                tup._2.forEach(pq::add);
+                while (pq.size() > 10) pq.poll();
+                List<Tuple2<String, Long>> sup = new ArrayList<>();
+                while (!pq.isEmpty()) sup.add(0, pq.poll());
+                return new Tuple2<>(tup._1, sup);
+            })
+            .sortByKey()
+            .collect()
+            .forEach(System.out::println);
+//            .aggregateByKey(new PriorityQueue<Tuple2<String, Long>>((t1, t2) -> t1._2.compareTo(t2._2)),
+//                (elAcumuladorQueVaASalir, laTuplaQueVieneEntrando) -> {
+//                    if (elAcumuladorQueVaASalir.size() < 10) {
+//                        elAcumuladorQueVaASalir.add(laTuplaQueVieneEntrando);
+//                    } else {
+//                        if (elAcumuladorQueVaASalir.peek()._2 < laTuplaQueVieneEntrando._2) {
+//                            elAcumuladorQueVaASalir.poll();
+//                            elAcumuladorQueVaASalir.add(laTuplaQueVieneEntrando);
+//                        }
+//                    }
+//                    return elAcumuladorQueVaASalir;
+//                }, (pq1, pq2) -> {
+//                    while (!pq2.isEmpty()) {
+//                        pq1.add(pq2.poll());
+//                    }
+//
+//                    while (pq1.size() > 10) {
+//                        pq1.poll();
+//                    }
+//                    return pq1;
+//                })
+    }
+}
