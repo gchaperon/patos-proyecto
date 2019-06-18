d5ad12d40e321ecb05cc18ed838eaa6dd803ba9b
gabriel <gabrielchaperonb@gmail.com>
2019-06-17 20:04:45 -0400

    last minute fixes.

--- java-apps/src/main/java/MostAnsweredRoots.java
+++ java-apps/src/main/java/MostAnsweredRoots.java
@@ -11,8 +11,8 @@ public class MostAnsweredRoots {
 
         JavaSparkContext spark = new JavaSparkContext(new SparkConf().setAppName("MostAnsweredRoots"));
 
-        JavaRDD<String> inputChildsRDD = spark.textFile(childsPath);
-        JavaRDD<String> inputsRootsRDD = spark.textFile(rootsPath);
+        JavaRDD<String> inputChildsRDD = spark.textFile(childsPath).distinct();
+        JavaRDD<String> inputsRootsRDD = spark.textFile(rootsPath).distinct();
 
         JavaPairRDD<String, Integer> childParentRDD = inputChildsRDD.mapToPair(
                 line -> new Tuple2<>(
@@ -29,10 +29,15 @@ public class MostAnsweredRoots {
         );
 
         JavaPairRDD<String, Tuple2<String, Integer>> rootTitleAndOccurrences = idAndTitleRDD.join(childParentRDD);
+        rootTitleAndOccurrences
+                .mapToPair(tup -> new Tuple2<>(tup._2._2, tup._2._1))
+                .sortByKey(false)
+                .take(10)
+                .forEach(System.out::println);
 
-        JavaPairRDD<String, Integer> r = rootTitleAndOccurrences.mapToPair(tup -> tup._2);
-
-        r.sortByKey(false).foreach(System.out::println);
+//        JavaPairRDD<String, Integer> r = rootTitleAndOccurrences.mapToPair(tup -> tup._2);
+//
+//        r.sortByKey(false).take(10).forEach(System.out::println);
     }
 
 }
