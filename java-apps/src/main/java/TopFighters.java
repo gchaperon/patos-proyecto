bcd35a82f5f01c6f7c19097c8fe4e67ea608e330
gabriel <gabrielchaperonb@gmail.com>
2019-06-18 19:21:37 -0400

    Why The Fuck?

--- java-apps/src/main/java/TopFighters.java
+++ java-apps/src/main/java/TopFighters.java
@@ -1,15 +1,9 @@
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.codehaus.janino.Java;
 import scala.Tuple2;
 import scala.Tuple3;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
 
 public class TopFighters {
     public static void main(String[] args) {
@@ -18,72 +12,79 @@ public class TopFighters {
 
         JavaSparkContext spark = new JavaSparkContext(new SparkConf().setAppName("Count +1/-1"));
 
+        /* Se cargan los datos de los root */
         JavaRDD<String> inputRootsRDD = spark.textFile(rootsPath);
+        /* (id, (autor, id_parent, id_thr)) */
         JavaPairRDD<String, Tuple3<String, String, String>> fatherItselfRDD = inputRootsRDD.mapToPair(
-                line -> {
-
-                    return new Tuple2<>(line.split("\t")[0], // id de raiz
-                            new Tuple3<>(line.split("\t")[2],"-"+line.split("\t")[0],line.split("\t")[0]));
-                }
+                line -> new Tuple2<>(line.split("\t")[0], // id de raiz
+                        new Tuple3<>(line.split("\t")[2], "-" + line.split("\t")[0], line.split("\t")[0]))
         );
 
-
-
+        /* Se cargan los datos de los childs */
         JavaRDD<String> inputChildsRDD = spark.textFile(childsPath);
+        /* (id, (autor, id_parent, id_thr)) */
         JavaPairRDD<String, Tuple3<String, String, String>> childAndFatherRDD = inputChildsRDD.mapToPair(
-                line -> {
-
-                    return new Tuple2<>(line.split("\t")[0], // id del comentario
-                    new Tuple3<>(line.split("\t")[3],line.split("\t")[2],line.split("\t")[1]));
-                }
+                line -> new Tuple2<>(line.split("\t")[0], // id del comentario
+                        new Tuple3<>(line.split("\t")[3], line.split("\t")[2], line.split("\t")[1]))
         );
 
-        JavaPairRDD<String, Tuple3<String, String, String>> fatherFirstRDD = childAndFatherRDD.mapToPair(
-                row -> new Tuple2<>(row._2._2(), new Tuple3<>(row._2._1(),row._1,row._2._3()))
-        );
-
+        /*Se crea un rdd grandote uniendo los dos anteriores
+         * (id, (autor, id_parent, id_thr)) */
         JavaPairRDD<String, Tuple3<String, String, String>> allRDD = childAndFatherRDD.union(fatherItselfRDD);
 
-        JavaPairRDD<String, Tuple2<Tuple3<String, String, String>,Tuple3<String, String, String>>> firstJoin = allRDD.join(fatherFirstRDD);
+        /*Se cambia la llave del rdd de childs
+         * (id_parent, (autor, id, id_thr))*/
+        JavaPairRDD<String, Tuple3<String, String, String>> fatherFirstRDD = childAndFatherRDD.mapToPair(
+                tup -> new Tuple2<>(tup._2._2(), new Tuple3<>(tup._2._1(), tup._1, tup._2._3()))
+        );
 
-        firstJoin.mapToPair( row -> new Tuple2<>(
-                row._2._2._2(),
-                new Tuple2<>(
-                        row._2._1(),
-                        row._2._2()
-
-                )))
+        /*Se realiza el primer join entre el rdd de todos con el rdd de hijos que tienen
+         * la id de parent como llave
+         * (id/id_parent, ((autor, id_parent, id_thr), (autor, id, id_thr)))*/
+        JavaPairRDD<
+                String,
+                Tuple2<
+                        Tuple3<String, String, String>,
+                        Tuple3<String, String, String>
+                        >
+                > firstJoin = allRDD.join(fatherFirstRDD);
+        
+        /*caleta de weas*/
+        firstJoin
+                /* Se mapea para cambiar la llave
+                * (id {* de la segunda columna}, ((autor, id_parent, id_thr), (autor, id, id_thr)))*/
+                .mapToPair(tup -> new Tuple2<>(
+                        tup._2._2._2(),
+                        tup._2())
+                )
+                /*Ahora se hace el join para ver quienes le respondieron a los
+                * de la segunda columna
+                * el formato de este esta brigido*/
                 .join(fatherFirstRDD)
-                .mapToPair( row -> new Tuple2<>(
-                   row._2._2._3(),
-                   new Tuple3<>(row._2._1._1._1(), row._2._1._2._1(), row._2._2._1())
+                /*(id_thr, (weon1, weon2, weon3))*/
+                .mapToPair(tup -> new Tuple2<>(
+                        tup._2._2._3(),
+                        new Tuple3<>(tup._2._1._1._1(), tup._2._1._2._1(), tup._2._2._1())
                 ))
-                .filter(row -> (row._2._1().equals(row._2._3()) && !row._2._1().equals(row._2._3())))
-                .mapToPair(row -> {
-                    String f = row._2._1();
-                    String s = row._2._2();
-                    String u;
-                    String d;
-                    if (f.compareTo(s) < 0){
-                        u = f;
-                        d = s;
-                    } else {
-                        u = s;
-                        d = f;
-                    }
-                    return new Tuple2<>(row._1, new Tuple2<>(u,d));
-                })
+                /*Ahora se filtran tal que el primero sea igual al ultimo y distinto al segundo*/
+                .filter(tup -> (tup._2._1().equals(tup._2._3()) && !tup._2._1().equals(tup._2._2())))
+                /*Se mapea de una tupla con el id_thr y una tupla con los wm que discutieron, ordenados
+                * dentro de la tupla por orden alfabetico */
+                .mapToPair(tup -> new Tuple2<>(tup._1, new Tuple2<>(tup._2._1(), tup._2._2())))
+                .mapToPair(tup -> new Tuple2<>(
+                        tup._1,
+                        tup._2._1().compareTo(tup._2._2()) < 0 ? tup._2 : tup._2.swap()
+                ))
+                /*Se deja unasola ocurrencia por thread*/
                 .distinct()
-                .mapToPair(row -> new Tuple2<>(row._2,1))
+                /*Ahora se cuentan*/
+                .mapToPair(tup -> new Tuple2<>(tup._2, 1))
                 .reduceByKey(Integer::sum)
-                .mapToPair(row -> new Tuple2<>(row._2,row._1))
+                // .mapToPair(row -> new Tuple2<>(row._2, row._1))
+                .mapToPair(tup -> tup.swap())
                 .sortByKey(false)
                 .take(20)
                 .forEach(System.out::println);
-
-                //take(10).forEach(System.out::println);
-
-
     }
 }
 
